5508dc69becb755dc5d4b02d0af669768e7c65ee
market-predictions <darko.desktop@gmail.com>
2026-05-31 23:07:28 +0200

    Add close engine adapters package

---
+++ pricing/close_engine/adapters/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from pricing.close_engine.adapters.deutsche_boerse import DeutscheBoerseAdapter
+from pricing.close_engine.adapters.euronext import EuronextAdapter
+
+__all__ = ["DeutscheBoerseAdapter", "EuronextAdapter"]
